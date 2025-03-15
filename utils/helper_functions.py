9c19bfbb4d549fb8b6265e78b7d82c3e9a598fe7
Jeff Watchorn <jeffreydwatchorn@gmail.com>
2025-03-15 13:16:42 -0400

    clean up helper functions and professionalize docstring

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -2,25 +2,35 @@
 from scipy.stats import chi2_contingency, spearmanr
 import pandas as pd
 
-def compute_year_correlation(df_counts, start_year, end_year):
-    ''' This function creates a series from a year-frequency value counts object and returns a zero filled sorted version, along with the correlation statistics
-    parameters:
-    df_counts: pd.Series
-        A pandas Series with years as index and frequency counts as values
+def compute_year_correlation(df_counts : pd.Series, start_year : int, end_year : int) -> tuple:
+    ''' This function creates a series from a year-frequency value counts object and returns a zero 
+    filled sorted version, along with the correlation statistics
+    
+    Parameters
+    ----------
 
-    start_year: int
-        The starting year for the correlation analysis
+        df_counts: pd.Series
+            A pandas Series with years as index and frequency counts as values
 
-    end_year: int
-        The ending year for the correlation analysis
+        start_year: int
+            The starting year for the correlation analysis
 
-    returns:
+        end_year: int
+            The ending year for the correlation analysis
+
+    Returns
+    -------
+
         tuple containing:
-        - filled_counts (pd.Series): The series with missing years filled with zero counts
-        - rho (float): The Spearman correlation coefficient
-        - p (float): The p-value corresponding to the Spearman correlation
-
-
+        
+        filled_counts: pd.Series
+            The series sorted ascending with missing years filled with zero counts
+        
+        rho: np.float64
+            The Spearman correlation coefficient
+        
+        p: np.float64
+            The p-value corresponding to the Spearman correlation
     '''
 
     if start_year>end_year:
